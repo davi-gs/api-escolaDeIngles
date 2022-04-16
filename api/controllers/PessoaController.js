103abc4353913fd344a6341edc8783bd1fefb8c9
David <davidpvh2020@gmail.com>
2022-04-16 00:15:29 -0400

    finalizando o crud

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -70,6 +70,47 @@ class PessoaController {
         }
     }
 
+    static async criarMatricula(req, res) {
+        const { estudanteId } = req.params
+        const novaMatricula = {... req.body, estudante_id: Number(estudanteId)}
+        try {
+            const novaMatriculaCriada = await database.Matriculas.create(novaMatricula)
+            return res.status(200).json(novaMatriculaCriada)
+
+        } catch (error) {
+            return res.status(500).json(error.message)
+        } 
+    }
+
+    static async atualizaMatricula(req, res) {
+        const { estudanteId, matriculaId } = req.params
+        const novasInfos = req.body
+        try {
+            await database.Matriculas.update(novasInfos, { 
+                where: {
+                    id: Number(matriculaId),
+                    estudante_id: Number(estudanteId)
+                }
+            })
+            const matriculaAtualizada = await database.Matriculas.findOne({
+                where: {id: Number(matriculaId)}
+            })
+            return res.status(200).json(matriculaAtualizada)
+
+        } catch (error){
+            return res.status(500).json(error.message)
+        }
+    }
+
+    static async apagaMatricula(req, res) {
+        const { estudanteId, matriculaId } = req.params
+        try {
+            await database.Matriculas.destroy({ where: { id: Number(matriculaId) }})
+            return res.status(200).json({ mensagem: `O id ${matriculaId} foi deletado` })
+        }catch (error) {
+        return res.status(500).json(error.message)
+        }
+    }
 }
 
 module.exports = PessoaController
