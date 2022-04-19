c027424901f546b023828a80a21dc0af23b23b74
David <davidpvh2020@gmail.com>
2022-04-18 23:46:43 -0400

    adicionando scope de associação

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -9,6 +9,8 @@ router.get('/pessoas/todos', PessoaController.pegaTodasAsPessoas)
 
 router.get('/pessoas/:id', PessoaController.pegaUmaPessoa)
 
+router.get('/pessoas/:estudanteId/matricula', PessoaController.pegaMatriculas)
+
 router.post('/pessoas', PessoaController.criarPessoa)
 
 router.put('/pessoas/:id', PessoaController.atualizaPessoa)
